cfb17f63c8246a8fe48648c846c7c38ad41a3d13
Akshat_Sharma-15 <akshatsharma152002@gmail.com>
2025-01-23 01:32:00 +0530

    Deleted

--- Assignment-2_22-Jan-2025/Assignment-1/Kilometers_to_miles.java
+++
@@ -1,8 +0,0 @@
-public class Kilometers_to_miles {
-    public static void main(String[] args) {
-        // Converting distance in kilometers into miles.
-        double dist = (double)(10.8 * 1.6);
-
-        System.out.println("Distance in miles is: " + dist + " miles");
-    }
-}
